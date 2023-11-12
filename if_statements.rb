820e27b9ccf4a1c7f1ce1b68aad589df7f587490
Odell McFarland IV <odell.mac4@gmail.com>
2023-11-12 11:51:35 -0800

    This commit will tell user if they have enough ingredients for pizza

--- if_statements.rb
+++ if_statements.rb
@@ -64,5 +64,10 @@ end
 # Experiment with manipulating the value held within both variables
 # to make sure all above conditions output what you expect.
 
-cups_of_flour = 1
+cups_of_flour = 3
 has_sauce = true
+if cups_of_flour < 2 || has_sauce == false
+  p "I cannot make pizza"
+else
+  p "I can make pizza"
+end
